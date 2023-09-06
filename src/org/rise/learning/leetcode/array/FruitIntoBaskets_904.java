25ac4095a98fd0fd34c30a27b26d964957143037
Jiang, Zhanpeng <Zhanpeng.Jiang@externals.adidas.com>
2023-09-06 23:21:57 +0800

    feat(leetcode): 904. 水果成篮
    双指针，滑动窗口题。本质上就是求数组中出现的最多2位重复的子序列的最大长度。
    
     关键在于，需要用Map去记录keyed by 元素值，valued by 出现次数。因为在移动slow缩小滑窗的过程中，我们需要借助Map完成出现次数的--减少操作，并且在减为0时，从Map中remove掉。
    
     为什么不用Set来统计，就是因为用Set，我们不知道slow移动到哪个下标才算是保留2位无重，所以需要用Map记录一下出现次数。

---
+++ src/org/rise/learning/leetcode/array/FruitIntoBaskets_904.java
@@ -0,0 +1,31 @@
+package org.rise.learning.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>https://leetcode.cn/problems/fruit-into-baskets/description/</p>
+ * 904. 水果成篮
+ *
+ * @author zhanpeng.jiang@hand-china.com 2023/9/6
+ */
+public class FruitIntoBaskets_904 {
+    public int totalFruit(int[] fruits) {
+        int maxTotalFruits = 0;
+        int slow = 0;
+        Map<Integer, Integer> appearCountByFruit = new HashMap<>(fruits.length);
+        for (int fast = 0; fast < fruits.length; fast++) {
+            appearCountByFruit.put(fruits[fast], appearCountByFruit.getOrDefault(fruits[fast], 0) + 1);
+            while (appearCountByFruit.size() > 2) {
+                int currentFruitAppearCount = appearCountByFruit.get(fruits[slow]) - 1;
+                appearCountByFruit.put(fruits[slow], currentFruitAppearCount);
+                if (currentFruitAppearCount == 0) {
+                    appearCountByFruit.remove(fruits[slow]);
+                }
+                slow++;
+            }
+            maxTotalFruits = Math.max(maxTotalFruits, fast - slow + 1);
+        }
+        return maxTotalFruits;
+    }
+}
